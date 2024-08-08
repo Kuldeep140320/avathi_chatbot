c6c313d395dcbcf7e2252b8a35c0538f17f1dba2
Kuldeep Gothwal <kuldeep.gothwal@avathi.com>
2024-08-08 17:19:16 +0530

    udpatse

--- app/models/llm.py
+++ app/models/llm.py
@@ -1,4 +1,4 @@
 from langchain_openai import ChatOpenAI
-from config import OPENAI_API_KEY, MODEL_NAME
+from app.config import OPENAI_API_KEY, MODEL_NAME
 
 llm = ChatOpenAI(model=MODEL_NAME, api_key=OPENAI_API_KEY, temperature=0.5)
