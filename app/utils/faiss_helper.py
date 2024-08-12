4511d9f461cf7b2b724e53fb9059dd4942c14c63
Kuldeep Gothwal <kuldeep.gothwal@avathi.com>
2024-08-12 15:48:02 +0530

    onle name of exp

--- app/utils/faiss_helper.py
+++ app/utils/faiss_helper.py
@@ -18,71 +18,71 @@ def clean_html(raw_html):
         return cleantext.strip()
     return "N/A"
 
-def fetch_data_from_table(table_name: str, fields: List[str]) -> pd.DataFrame:
-    """Fetch data from the specified table in the database."""
-    conn = get_db_connection()
-    query = f'SELECT {", ".join(fields)} FROM "{table_name}" WHERE is_active = TRUE and display_priority < 20'
-    
-    df = pd.read_sql(query, conn)
-    conn.close()
-    return df
-# def fetch_data_from_table(table_name: str) -> pd.DataFrame:
+# def fetch_data_from_table(table_name: str, fields: List[str]) -> pd.DataFrame:
 #     """Fetch data from the specified table in the database."""
 #     conn = get_db_connection()
-#     query = """
-#     SELECT 
-#         e.primary_key AS eoexperience_primary_key,
-#         e.name AS eoexperience_name,
-#         e.description AS eoexperience_description,
-#         e.location,
-#         e.address,
-#         e.is_active AS eoexperience_is_active,
-#         e.is_stay,
-#         e.price,
-#         e.faqs AS eoexperience_faqs,
-#         e.eoproperty_primary_key,
-#         e.card_image_primary_key,
-#         e.is_choose_stay,
-#         e.lkdestination_primary_key,
-#         e.question,
-#         e.display_priority AS eoexperience_display_priority,
-#         e.expert_summery,
-#         e.things_to_note,
-#         e.things_to_do,
-#         e.short_name,
-#         e.exp_type,
-#         e.reviews_count,
-#         e.avg_rating,
-#         l.primary_key AS lkdestination_primary_key,
-#         l.name AS lkdestination_name,
-#         l.is_active AS lkdestination_is_active,
-#         p.primary_key AS eoplace_primary_key,
-#         p.lkdestination_primary_key AS eoplace_lkdestination_primary_key,
-#         p.place_title,
-#         p.place_description,
-#         p.is_active AS eoplace_is_active,
-#         p.faqs AS eoplace_faqs
-#     FROM 
-#         public.eoexperience e
-#     LEFT JOIN 
-#         public.lkdestination l ON e.lkdestination_primary_key = l.primary_key
-#     LEFT JOIN 
-#         public.eoplace p ON l.primary_key = p.lkdestination_primary_key
-#     WHERE 
-#         e.is_active = TRUE AND 
-#         l.is_active = TRUE AND 
-#         p.is_active = TRUE
-#     ORDER BY 
-#         e.primary_key ASC;
-#     """
+#     query = f'SELECT {", ".join(fields)} FROM "{table_name}" WHERE is_active = TRUE and display_priority < 20'
+    
 #     df = pd.read_sql(query, conn)
 #     conn.close()
 #     return df
+def fetch_data_from_table(table_name: str) -> pd.DataFrame:
+    """Fetch data from the specified table in the database."""
+    conn = get_db_connection()
+    query = """
+    SELECT 
+        e.primary_key AS eoexperience_primary_key,
+        e.name AS eoexperience_name,
+        e.description AS eoexperience_description,
+        e.location,
+        e.address,
+        e.is_active AS eoexperience_is_active,
+        e.is_stay,
+        e.price,
+        e.faqs AS eoexperience_faqs,
+        e.eoproperty_primary_key,
+        e.card_image_primary_key,
+        e.is_choose_stay,
+        e.lkdestination_primary_key,
+        e.question,
+        e.display_priority AS eoexperience_display_priority,
+        e.expert_summery,
+        e.things_to_note,
+        e.things_to_do,
+        e.short_name,
+        e.exp_type,
+        e.reviews_count,
+        e.avg_rating,
+        l.primary_key AS lkdestination_primary_key,
+        l.name AS lkdestination_name,
+        l.is_active AS lkdestination_is_active,
+        p.primary_key AS eoplace_primary_key,
+        p.lkdestination_primary_key AS eoplace_lkdestination_primary_key,
+        p.place_title,
+        p.place_description,
+        p.is_active AS eoplace_is_active,
+        p.faqs AS eoplace_faqs
+    FROM 
+        public.eoexperience e
+    LEFT JOIN 
+        public.lkdestination l ON e.lkdestination_primary_key = l.primary_key
+    LEFT JOIN 
+        public.eoplace p ON l.primary_key = p.lkdestination_primary_key
+    WHERE 
+        e.is_active = TRUE AND 
+        l.is_active = TRUE AND 
+        p.is_active = TRUE
+    ORDER BY 
+        e.primary_key ASC;
+    """
+    df = pd.read_sql(query, conn)
+    conn.close()
+    return df
 
 def create_documents_from_db(table_name: str, fields: List[str]) -> List[Document]:
     # print(table_name,fields)
     # sys.exit()
-    df = fetch_data_from_table(table_name ,fields)
+    df = fetch_data_from_table(table_name)
     # print(df)
     # sys.exit()
     chunk_size = 1500
@@ -90,13 +90,14 @@ def create_documents_from_db(table_name: str, fields: List[str]) -> List[Documen
     
     for _, row in df.iterrows():
         content = (
-            f"Name: {row['name']}"
-            f"Location name: {clean_html(row['description'])}\n"
-            f"Name: {row['name']}"
+            f"Name: {row['eoexperience_name']}"
+            # f"Location name: {clean_html(row['description'])}\n"
+            # f"Name: {row['name']}"
             f"Place Title: {clean_html(row['location'])}"
             f"Description: {clean_html(row['address'])}"
+            f"lkdestination_name: {row['lkdestination_name']}"
             f"Location: {clean_html(row['price'])}"
-            # f"Address: {clean_html(row['address'])}"
+            f"place_title: {clean_html(row['place_title'])}\n"
             # f"Is Stay: {'Yes' if row['is_stay'] else 'No'}"
             # f"Price: {row['price'] if row['price'] else 'N/A'}"
             # f"eoexperience_faqs: {clean_html(row['eoexperience_faqs'])}\n"
@@ -120,17 +121,17 @@ def create_documents_from_db(table_name: str, fields: List[str]) -> List[Documen
         if current_chunk:
             chunks.append(current_chunk.strip())
 
-        display_priority = row['display_priority'] if pd.notnull(row['display_priority']) else 100
+        display_priority = row['eoexperience_display_priority'] if pd.notnull(row['eoexperience_display_priority']) else 100
 
         for chunk in chunks:
             embedding = instruct_embeddings.embed_query(chunk)
             doc = Document(
                 page_content=chunk,
                 metadata={
-                    "eoexperience_primary_key": row['primary_key'],
-                    "eoexperience_name": row['name'],
-                    "lkdestination_primary_key": row['location'],
-                    # "lkdestination_name": row['lkdestination_name'],
+                    "eoexperience_primary_key": row['eoexperience_primary_key'],
+                    "eoexperience_name": row['eoexperience_name'],
+                    # "lkdestination_primary_key": row['location'],
+                    "lkdestination_name": row['lkdestination_name'],
                     # "eoplace_primary_key": row['eoplace_primary_key'],
                     # "eoplace_lkdestination_primary_key": row['eoplace_lkdestination_primary_key'],
                     # "eoplace_place_title": row['place_title'],
@@ -139,6 +140,9 @@ def create_documents_from_db(table_name: str, fields: List[str]) -> List[Documen
                 },
                 embedding=embedding
             )
+            # print('dd')
+            # print(doc)
+            # sys.exit()
             documents.append(doc)
     return documents
 
